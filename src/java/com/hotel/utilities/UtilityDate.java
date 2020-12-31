35e7a40ad48d29b57ffe3066a0599f6524449e4e
jaidernperez <jaider.perez@usantoto.edu.co>
2020-12-30 19:58:56 -0500

    feat: add utility date

---
+++ src/java/com/hotel/utilities/UtilityDate.java
@@ -0,0 +1,19 @@
+package com.hotel.utilities;
+
+import java.util.Date;
+
+public class DateReservation {
+
+    public static java.sql.Date generateDate() {
+        Date currentDate = new java.util.Date();
+        return new java.sql.Date(currentDate.getTime());
+    }
+
+    public static boolean isGreaterEndDate(Date startDate, Date endDate){
+        return startDate.before(endDate);
+    }
+
+    public static java.sql.Date convertToSqlDate(Date date){
+        return new java.sql.Date(date.getTime());
+    }
+}
